44ded59899de5ed790ae65b2e67ae4ff339dc23f
Shailendra Chahar <shailendra.chahar@gmail.com>
2020-04-28 00:49:42 +0530

    API Auth

---
+++ DatingApp.API/DTO/UserForLoginDTO .cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.DTO
+{
+    public class UserForLoginDTO
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength=4,ErrorMessage="Must specify password between 4 and 8 characters.")]
+        public string Password { get; set; }
+    }
+}
