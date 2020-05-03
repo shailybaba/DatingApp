26bc4601564edcead60c4dc9a17297732540b5df
Shailendra Chahar <shailendra.chahar@gmail.com>
2020-05-04 00:54:45 +0530

    Extended API

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.DTO;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User,UserForListDTO>()
+                .ForMember(dest => dest.PhotoUrl, 
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault( p => p.IsMain).Url))
+                .ForMember(dest => dest.Age,opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+           
+            CreateMap<User,UserForDetailedDTO>()
+                .ForMember(dest => dest.PhotoUrl, 
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault( p => p.IsMain).Url))
+                .ForMember(dest => dest.Age,opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            
+            CreateMap<Photo,PhotosForDetailedDTO>();
+        }
+    }
+}
